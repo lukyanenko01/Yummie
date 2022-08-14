1e0925696989ea7f39054cfdc46c6a61fa5bb5f2
lukyanenko01 <v.lukyanenko1008@gmail.com>
2022-08-14 12:24:57 +0300

    «next»

---
+++ Yummie/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Lukich on 14.08.2022.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+    
+    func setup(category: DishCategory) {
+        
+        
+        
+    }
+}
---
+++ Yummie/Models/DishCategory.swift
@@ -0,0 +1,13 @@
+//
+//  DishCategory.swift
+//  Yummie
+//
+//  Created by Lukich on 14.08.2022.
+//
+
+import UIKit
+
+struct DishCategory {
+    let id, name, image: String
+    
+}
